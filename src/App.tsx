8a6568752b47597516bc8838648a6945d130ac88
권은서 <kwonenseo@gmail.com>
2022-03-24 16:19:55 +0900

    feat: Made theme with TypeScript, styled-components
    styled.d.ts => extend theme props
    theme.ts => Organized lightTheme and darkTheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,18 @@
 import React, { useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+	background-color: ${props => props.theme.bgColor};
+`;
+const H1 = styled.h1`
+	color: ${props => props.theme.textColor};
+`;
 
 function App() {
-	const [value, setValue] = useState("");
-	// 어떤 종류의 element가 이 onChange 이벤트를 발생시킬지 특정할 수 있다.ex)<HTMLInputElement>
-	const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-		const {
-			currentTarget: {value},
-		} = event;
-		setValue(value);
-	}
-	const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-		event.preventDefault();
-		console.log("Hello", value);
-	}
 	return (
-		<div>
-			<form onSubmit={onSubmit}>
-				<input 
-				value={value} 
-				onChange={onChange} 
-				type="text" 
-				placeholder="username" />
-				<button>Log in</button>
-			</form>
-    </div>
+		<Container>
+			<H1>Protected</H1>
+    </Container>
 	);
 }
 
