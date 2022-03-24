f71a56635af39ce443c51469933e2d9311fd6e31
권은서 <kwonenseo@gmail.com>
2022-03-24 15:54:03 +0900

    Study how to use form event with TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import styled from "styled-components";
-import Circle from "./Circle";
+import React, { useState } from "react";
 
 function App() {
+	const [value, setValue] = useState("");
+	// 어떤 종류의 element가 이 onChange 이벤트를 발생시킬지 특정할 수 있다.ex)<HTMLInputElement>
+	const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+		const {
+			currentTarget: {value},
+		} = event;
+		setValue(value);
+	}
+	const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		console.log("Hello", value);
+	}
 	return (
 		<div>
-			<Circle borderColor="tomato" bgColor="teal" />
-			<Circle bgColor="pink" text="hi im props" />
+			<form onSubmit={onSubmit}>
+				<input 
+				value={value} 
+				onChange={onChange} 
+				type="text" 
+				placeholder="username" />
+				<button>Log in</button>
+			</form>
     </div>
 	);
 }
