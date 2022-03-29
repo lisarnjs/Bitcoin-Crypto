2cfb05a0f7260f60bd5525f0023ab3c77ef1e160
권은서 <kwonenseo@gmail.com>
2022-03-29 16:11:58 +0900

    Setup for react-router-dom & react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,8 @@
-import React, { useState } from "react";
-import styled from "styled-components";
-
-const Container = styled.div`
-	background-color: ${props => props.theme.bgColor};
-`;
-const H1 = styled.h1`
-	color: ${props => props.theme.textColor};
-`;
-
-interface DummyProps {
-	text?: string;
-}
-
-function Dummy({text = "bye"}: DummyProps) {
-	return <H1>{text}</H1>
-}
+import Router from "./Router";
 
 function App() {
-	const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-		console.log(event);
-	}
 	return (
-		<Container>
-			<Dummy text="hello"  />
-			<button onClick={onClick}>click me</button>
-    </Container>
+		<Router />
 	);
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
 import App from './App';
 import "./App.css";
-import { darkTheme, lightTheme } from './theme';
+import { theme } from './theme';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
   </React.StrictMode>,
