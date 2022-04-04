1db99d501c682502233c45e1726384f7670dfc04
권은서 <kwonenseo@gmail.com>
2022-04-04 14:39:32 +0900

    Link로 object를 이용한 state를 넘겨줄 수 있다.
    **react-router-dom v5**
    Home화면을 거치지 않고 바로 세부 코인 페이지로 들어왔을 경우 state를
    Home에서부터 넘겨받지 못했기 때문에 "Loading..."문구를 보여주도록 설정

--- src/routes/Conis.tsx
+++ src/routes/Conis.tsx
@@ -23,7 +23,8 @@ const Coin = styled.li`
   border-radius: 15px;
   margin-bottom: 10px;
   a {
-    display: block;
+    display: flex;
+    align-items: center;
     padding: 20px;
     transition: color .2s ease-in;
   }
@@ -42,6 +43,12 @@ const Title = styled.h1`
 const Loader = styled.span`
   display: block;
   text-align: center;
+`;
+
+const Img = styled.img`
+  width: 35px;
+  height: 35px;
+  margin-right: 10px;
 `;
 
 interface CoinInterface {
@@ -76,7 +83,13 @@ function Coins() {
       :<CoinsList>
           {coins.map(coin => 
           <Coin key={coin.id}>
-            <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+            <Link to={{
+              pathname: `/${coin.id}`,
+              state: { name: coin.name },
+            }}>
+              <Img src={`https://cryptocurrencyliveprices.com/img/${coin.id}.png`} />
+              {coin.name} &rarr;
+            </Link>
           </Coin>)}
       </CoinsList>}
     </Container>
