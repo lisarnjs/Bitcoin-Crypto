d1021a6ab28de34b69dd703eca87053a856e81c1
권은서 <kwonenseo@gmail.com>
2022-05-09 14:57:18 +0900

    Install Apex chart library

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,12 +1,23 @@
 import { useQuery } from "react-query";
 import { fetchCoinHistory } from "../api";
 
+interface IHistorical {
+  time_open: string;
+  time_close: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  market_cap: number;
+}
+
 interface ChartProps {
   coinId: string;
 }
 
 function Chart({ coinId }: ChartProps) {
-  const { isLoading, data } = useQuery(["price", coinId], () =>
+  const { isLoading, data } = useQuery<IHistorical[]>(["price", coinId], () =>
     fetchCoinHistory(coinId)
   );
   return <h1>Chart</h1>;
