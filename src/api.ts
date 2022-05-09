de3eab98623d5d2fac9b574e41f1400958bd431f
권은서 <kwonenseo@gmail.com>
2022-05-09 14:49:53 +0900

    Get ohlcv api and use useQuery hook

--- src/api.ts
+++ src/api.ts
@@ -15,3 +15,13 @@ export function fetchCoinTickers(coinId: string) {
     response.json()
   );
 }
+
+export function fetchCoinHistory(coinId: string) {
+  // Date.now() == milliseconds
+  // Date.now() / 1000 == seconds
+  const endDate = Math.floor(Date.now() / 1000); // 현재
+  const startDate = endDate - 60 * 60 * 24 * 7; // 일주일 전 (60s*60m*24h*7일 : 일주일을 초로 계산한 것)
+  return fetch(
+    `${BASE_URL}/coins/${coinId}/ohlcv/historical?start=${startDate}&end=${endDate}`
+  ).then((response) => response.json());
+}
