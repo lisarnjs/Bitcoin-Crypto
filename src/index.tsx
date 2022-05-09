be62690e77a4b0dc05cb1a54079fda2d615b7fe0
권은서 <kwonenseo@gmail.com>
2022-05-09 14:30:04 +0900

    Add react query devtools

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { ThemeProvider } from 'styled-components';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ThemeProvider } from "styled-components";
+import App from "./App";
 import "./App.css";
-import { theme } from './theme';
+import { theme } from "./theme";
 
+const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
